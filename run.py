204c905b858637d0e810102cde3f7ec77a1d1551
Njeneza François <njenezamed@gmail.com>
2016-09-30 09:31:28 +0000

    request_code method

--- run.py
+++ run.py
@@ -1,42 +1,53 @@
 import yaml
 from yowsup.registration import WACodeRequest
 
-phone = None
-cc = None
-mcc = None
-mnc = None
-code = None
-
-with open("config.yml") as config:
-    params = yaml.load(config)
-    phone = params["phone"]
-    cc = params["cc"]
-    mcc = params["mcc"]
-    mnc = params["mnc"]
-    code = params["code"]
+def foo():
+    phone = None
+    cc = None
+    mcc = None
+    mnc = None
+    code = None
 
 
-if phone is None:
-    print("phone is missing")
 
-if cc is None:
-    print("cc is missing")
+    with open("config.yml") as config:
+        params = yaml.load(config)
+        phone = params["phone"]
+        cc = params["cc"]
+        mcc = params["mcc"]
+        mnc = params["mnc"]
+        code = params["code"]
 
-if mcc is None:
-    print("mcc is missing")
 
-if mnc is None:
-    print("mnc is missing")
+    if phone is None:
+        print("phone is missing")
 
-if code is None:
-    print("code missing")
+    if cc is None:
+        print("cc is missing")
 
+    if mcc is None:
+        print("mcc is missing")
+
+    if mnc is None:
+        print("mnc is missing")
+
+    if code is None:
+        print("code missing")
+
+def request_code(cc, phone, mcc, mnc):
+    """
+    send a request for code
+    """
     #request code
-    request_code = WACodeRequest(cc, phone, mcc, mnc, mcc, mnc)
-    result = request_code.send()
-
+    req_code = WACodeRequest(cc, phone, mcc, mnc, mcc, mnc)
+    result = req_code.send()
     print(result)
 
-else:
-    #register
+    if result["status"] == "fail":
+        return (False, result["reason"])
+
+    else:
+        return (True, None)
+
+def register(cc, phone, password):
     pass
