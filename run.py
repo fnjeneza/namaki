bf31b6619132840246a625a2c6c0f4571a2c4a1f
Njeneza François <njenezamed@gmail.com>
2016-09-30 15:42:15 +0000

    namaki stack

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import yaml
 from yowsup.registration import WACodeRequest, WARegRequest
+from yowsup.stacks import YowStackBuilder
+from yowsup.layers.axolotl.props import PROP_IDENTITY_AUTOTRUST
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.auth import AuthError
+from layer import NamakiLayer
 
 CONFIG_FILE = "config"
 
@@ -42,6 +47,40 @@ def register(code):
         return (False, result["reason"])
 
     else:
+        with open(CONFIG_FILE, "w") as config_file:
+            config["pw"] = result["pw"]
+            config["loging"] = result["login"]
+            yaml.dump(config, config_file)
         return (True, None)
 
+class NamakiStack(object):
+    def __init__(self):
+        stackBuilder = YowStackBuilder()
 
+        self.stack = stackBuilder\
+                .pushDefaultLayers(True)\
+                .push(NamakiLayer)\
+                .build()
+
+        credentials = None
+
+        with open(CONFIG_FILE) as config_file:
+            config = yaml.load(config_file)
+            login = config["login"]
+            pw = config["pw"]
+            assert login is not None, "login must not be empty"
+            assert pw is not None, "pw must no be empty"
+            credentials = (login, pw)
+
+
+        self.stack.setCredentials(credentials)
+        self.stack.setProp(PROP_IDENTITY_AUTOTRUST)
+
+    def start(self):
+        print("starting...")
+        self.stack.broadcastEvent(YowLayerEvent(NamakiLayer.EVENT_START))
+
+        try:
+            self.stack.loop()
+        except AuthError as e:
+            print("AuthError reason %s" % e)
