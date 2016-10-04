aeabe3a6a94ea7afb23ba3250236633f5a3341c0
Njeneza François <njenezamed@gmail.com>
2016-10-04 17:29:07 +0000

    send message

--- layer.py
+++ layer.py
@@ -1,7 +1,18 @@
 from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.layers.protocol_receipts.protocolentities import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+
+suffix = "@s.whatsapp.net"
 
 class NamakiLayer(YowInterfaceLayer):
+
+    def phone_to_jid(self, phone):
+        return phone + "@s.whatsapp.net"
+
+
+    def has_jid(self, phone):
+        return phone.find(suffix) >= 0
+
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
@@ -17,3 +28,12 @@ class NamakiLayer(YowInterfaceLayer):
 
         #send ack
         self.toLower(receipt)
+
+
+    def send_message(self, phone, message):
+        if not self.has_jid(phone):
+            phone = self.phone_to_jid(phone)
+
+        out = TextMessageProtocolEntity(message.encode(),
+                to = phone)
+        self.toLower(out)
