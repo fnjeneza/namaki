76cb3a60b9e18ea570066afb50fd2ae64112fffa
Njeneza François <njenezamed@gmail.com>
2016-10-04 08:43:21 +0000

    send ack

--- layer.py
+++ layer.py
@@ -1,10 +1,15 @@
 from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
-from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities import OutgoingReceiptProtocolEntity
 
 class NamakiLayer(YowInterfaceLayer):
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
-        #message = messageProtocolEntity.getBody()
-        print("incoming message")
-        #print(message)
+        receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(),
+                messageProtocolEntity.getFrom(),
+                "read",
+                messageProtocolEntity.getParticipant())
+
+        message = messageProtocolEntity.getBody()
+        print("... %s" % message)
+        self.toLower(receipt)
