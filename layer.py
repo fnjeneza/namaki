9cabb30d70429c142c43b1d4d3410ca1b664e695
Njeneza François <njenezamed@gmail.com>
2016-10-05 08:36:29 +0000

    connection established

--- layer.py
+++ layer.py
@@ -2,17 +2,26 @@ from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.layers.protocol_receipts.protocolentities import OutgoingReceiptProtocolEntity
 from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
 
+
 suffix = "@s.whatsapp.net"
 
 class NamakiLayer(YowInterfaceLayer):
+    def __init(self):
+        self.connected = False
+        self.suffix = "@s.whatsapp.net"
+
 
     def phone_to_jid(self, phone):
-        return phone + "@s.whatsapp.net"
+        return phone + self.suffix
 
 
     def has_jid(self, phone):
         return phone.find(suffix) >= 0
 
+    @ProtocolEntityCallback("success")
+    def onSuccess(self, entity):
+        self.connected = True
+        print("ouha i'm connected")
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
