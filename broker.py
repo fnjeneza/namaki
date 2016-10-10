b17391961809e23e73bfdd5531f601aa0a7d3de1
Njeneza François <njenezamed@gmail.com>
2016-10-10 10:24:02 +0000

    setup receiver and sender

--- broker.py
+++ broker.py
@@ -1,12 +1,31 @@
 import zmq
+from threading import Thread
 
 class Broker:
     def __init__(self, bind_address="*", port="5557"):
         print("Broker initialisation...")
-        context = zmq.Context()
-        self.receiver_socket = context.socket(zmq.REP)
-        self.receiver_socket.bind("tcp://" + bind_address + ":" + port)
-        self.handle_message()
+        self.context = zmq.Context()
+        Thread(target=self._setup_receiver, args=(bind_address, port,)).start()
+        self.sender = self._setup_sender("localhost", "5558")
+
+
+    def _setup_receiver(self, address, port):
+        receiver_socket = self.context.socket(zmq.REP)
+        receiver_socket.bind("tcp://" + address + ":" + port)
+        while True:
+            msg = receiver_socket.recv()
+            print(msg)
+            receiver_socket.send_string("")
+
+    def _setup_sender(self, address, port):
+        sender_socket = self.context.socket(zmq.REQ)
+        sender_socket.connect("tcp://" + address + ":" + port)
+        return sender_socket
+
+    def send(self, message):
+        self.sender.send_string(message)
+        reply = self.sender.recv()
+        return reply
 
 
     def handle_message(self):
@@ -20,3 +39,4 @@ class Broker:
 
 if __name__ == "__main__":
     br = Broker()
+    br.send("server")
