f28b8897f334dda8ddb18055929a0d363b1fd681
Njeneza François <njenezamed@gmail.com>
2016-10-10 07:39:10 +0000

    default initialization parameter

--- broker.py
+++ broker.py
@@ -1,11 +1,11 @@
 import zmq
 
 class Broker:
-    def __init__(self):
+    def __init__(self, bind_address="*", port="5557"):
         print("Broker initialisation...")
         context = zmq.Context()
         self.sender = context.socket(zmq.PUSH)
-        self.sender.bind("tcp://*:5557")
+        self.sender.bind("tcp://" + bind_address + ":" + port)
 
     def send(self, message):
         print("sending message to client")
