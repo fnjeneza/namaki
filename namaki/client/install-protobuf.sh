f131ee2010520540a7a1d0ac108b8fb968b55c5f
Francois Njeneza <njenezamed@gmail.com>
2016-10-25 19:32:11 +0000

    use version 3.0.2

--- namaki/client/install-protobuf.sh
+++ namaki/client/install-protobuf.sh
@@ -8,22 +8,22 @@
 # The script had been tested on ubuntu 16.04
 #
 
-sudo apt install build-essential libtool
+sudo apt install -y build-essential libtool
 
 $PWD=`pwd`
 cd /tmp/
 
 #download protobuf release 3.1.0
-echo "download protobuf release 3.1.0"
+echo "download protobuf release 3.0.2"
 echo ""
-wget https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.tar.gz
+wget https://github.com/google/protobuf/releases/download/v3.0.2/protobuf-cpp-3.0.2.tar.gz
 
 echo ""
 echo "Extracting protobuf archive"
 echo ""
-tar xzf protobuf-cpp-3.1.0.tar.gz
+tar xzf protobuf-cpp-3.0.2.tar.gz
 
-cd protobuf-3.1.0/
+cd protobuf-3.0.2/
 
 ./configure
 make
