14b360d9771c6658f69f24d5e4aa30bc5b8fd4f3
Francois Njeneza <njenezamed@gmail.com>
2016-11-04 15:54:52 +0000

    database structure

---
+++ namaki/client/namaki/initialization.sql
@@ -0,0 +1,22 @@
+---
+--- Contact
+---
+CREATE TABLE IF NOT EXISTS contact (
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+---
+--- Message
+---
+CREATE TABLE IF NOT EXISTS message (
+    id INTEGER PRIMARY KEY,
+    body TEXT,
+    out BOOLEAN,
+    read BOOLEAN,
+    timestamp INTEGER,
+    contact_id VARCHAR(50),
+    FOREIGN KEY(contact_id) REFERENCES contact(id)
+);
+
+
