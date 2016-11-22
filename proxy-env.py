a8b84c86c9a36629e3033085e6274c73f9a20e1e
Francois Njeneza <njenezamed@gmail.com>
2016-11-22 15:02:50 +0000

    namespace Namaki::Proxy

--- proxy-env.py
+++ proxy-env.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import venv
-import os
+import os, sys
 import subprocess
 import shlex
 
@@ -21,17 +21,19 @@ def install(packages):
     cmd = shlex.split(cmd)
     subprocess.run(cmd)
 
-def install_yowsup():
-    yowsup = os.path.join(os.getcwd(), "3rd-party", "yowsup")
+def install_yowsup(path):
+    yowsup = os.path.join(path, "3rd-party", "yowsup")
     os.chdir(yowsup)
     cmd = "python setup.py install"
     cmd = shlex.split(cmd)
     subprocess.run(cmd)
 
 if __name__ == "__main__":
-    path = os.path.abspath("src/proxy")
+    origin = os.getcwd()
+    path = os.path.join(sys.argv[1], "proxy")
+    path = os.path.abspath(path)
     create_and_change_env(path)
     python = os.path.join(path, "bin","python")
     packages = ["pyzmq", "pyyaml"]
     install(packages)
-    install_yowsup()
+    install_yowsup(origin)
